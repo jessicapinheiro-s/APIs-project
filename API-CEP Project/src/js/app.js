53c9e359afc8c0b9f341dcc4fc67bba75809c183
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2023-05-28 16:45:49 -0300

    alguns ajustes no estilo

--- API-CEP Project/src/js/app.js
+++ API-CEP Project/src/js/app.js
@@ -17,21 +17,33 @@ document.getElementById('btPesquisarCep')
         fetch(urlApi).then(function (response) {
             response.json().then(mostrarEndereco);
         })
-
+        styleComponents();
     })
 
 function mostrarEndereco(dados) {
     let respostaCep = document.querySelector('.respostaCep');
-
+    
     if (dados.erro) {
         respostaCep.innerHTML = `Não foi possivel loaclizar o endereço..`
     } else {
-        respostaCep.innerHTML = `<p>${dados.logradouro}</p>
-                                <p>${dados.bairro}</p>
-                                <p>${dados.localidade} - ${dados.uf}</p>
-                                <p>DDD - ${dados.ddd}</p>`;
+        respostaCep.innerHTML = `<p><strong>Rua:</strong> ${dados.logradouro}</p>
+                                <p><strong>Bairro:</strong> ${dados.bairro}</p>
+                                <p><strong>Localidade:</strong> ${dados.localidade} - ${dados.uf}</p>
+                                <p><strong>DDD:</strong>  - ${dados.ddd}</p>`;
 
     }
+}
+
+function styleComponents (){
+    let containerResp = document.getElementById('container-resp');
+
+    containerResp.style.width = '20%';
+    containerResp.style.margin= '15px auto';
+    containerResp.style.padding= '15px 20px';
+    containerResp.style.borderRadius= '10px';
+    containerResp.style.border= '1px solid rgb(231, 231, 231)';
+    containerResp.style.boxShadow= '1px 3px 5px 3px rgba(230, 230, 230, 0.473)';
+
 }
 
 
